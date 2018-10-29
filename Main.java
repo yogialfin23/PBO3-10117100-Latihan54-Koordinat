e6a729f1e7227ad3637d41319c79c575beb094b7
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-10-29 19:18:59 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	WarnaKoordinat wk = new WarnaKoordinat(10, 4, "Jingga");
+
+        System.out.println("Warna Koordinat : " +wk.getNamaWarna());
+        System.out.println("Koordinat sumbu x : " +wk.getX()+ " Sumbu y : " +wk.getY());
+    }
+}
---
+++ WarnaKoordinat.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class WarnaKoordinat extends Koordinat{
+    public String namaWarna;
+
+    public WarnaKoordinat(int x, int y, String namaWarna) {
+        super(x, y);
+        this.namaWarna = namaWarna;
+    }
+
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+}
